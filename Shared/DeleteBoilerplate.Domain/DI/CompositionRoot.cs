336865f7cf3c07e16d3bbd183b6569bdc942ccdd
Mikhail Dervel <mikhail.dervel@deleteagency.com>
2019-07-24 11:33:28 +0500

    added Domain project

---
+++ Shared/DeleteBoilerplate.Domain/DI/CompositionRoot.cs
@@ -0,0 +1,13 @@
+﻿using DeleteBoilerplate.Domain.Repositories;
+using LightInject;
+
+namespace DeleteBoilerplate.Domain.DI
+{
+    public class CompositionRoot : ICompositionRoot
+    {
+        public void Compose(IServiceRegistry serviceRegistry)
+        {
+            serviceRegistry.RegisterScoped<IProjectRepository, ProjectRepository>();
+        }
+    }
+}
---
+++ Shared/DeleteBoilerplate.Domain/Repositories/ProjectRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CMS.DocumentEngine.Types.DeleteBoilerplate;
+using CMS.Helpers;
+using CMS.SiteProvider;
+
+namespace DeleteBoilerplate.Domain.Repositories
+{
+    public interface IProjectRepository
+    {
+        List<Project> GetAllProjects(string siteName = null);
+    }
+
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly string _projectCacheKey = "deleteboilerplate|project";
+
+        public List<Project> GetAllProjects(string siteName = null)
+        {
+            var result = new List<Project>();
+            if (siteName == null)
+            {
+                siteName = SiteContext.CurrentSiteName;
+            }
+            using (var cs = new CachedSection<List<Project>>(ref result, CacheHelper.CacheMinutes(siteName), true, _projectCacheKey))
+            {
+                if (cs.LoadData)
+                {
+                    result = ProjectProvider.GetProjects().OnSite(siteName).ToList();
+
+                    var cacheDependencies = new List<string>
+                    {
+                        $"nodes|{siteName}|{Project.CLASS_NAME}|all"
+                    };
+
+                    cs.Data = result;
+                    cs.CacheDependency = CacheHelper.GetCacheDependency(cacheDependencies);
+                }
+            }
+
+            return result;
+        }
+    }
+}
