4805430f8220b90ae5b4db4ef45ce724578d63f7
Alexey Nenazhenko <alexey.nenazhenko@deleteagency.com>
2019-07-26 18:33:40 +0500

    added seo url autocomplete module

---
+++ CMS/DeleteBoilerplate.Modules/DynamicRoutingModule.cs
@@ -0,0 +1,50 @@
+﻿using CMS;
+using CMS.DataEngine;
+using CMS.DocumentEngine;
+using DeleteBoilerplate.Modules;
+
+[assembly: RegisterModule(typeof(DynamicRoutingModule))]
+
+namespace DeleteBoilerplate.Modules
+{
+    public class DynamicRoutingModule : Module
+    {
+        public DynamicRoutingModule() : base("DynamicRouting")
+        {
+        }
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+
+            DocumentEvents.Update.Before += UpdateOnBefore;
+            DocumentEvents.Insert.Before += UpdateOnBefore;
+        }
+
+        private void UpdateOnBefore(object sender, DocumentEventArgs e)
+        {
+            if (e.Node.ContainsColumn("SeoUrl"))
+                this.EnsurePopulatedSeoField(e.Node);
+        }
+
+        private void EnsurePopulatedSeoField(TreeNode node)
+        {
+            var seoUrl = (string)node["SeoUrl"];
+            var updated = false;
+
+            if (string.IsNullOrWhiteSpace(seoUrl))
+            {
+                node["SeoUrl"] = node.NodeAliasPath;
+                updated = true;
+            }
+            else if (!seoUrl.StartsWith("/"))
+            {
+                node["SeoUrl"] = $"/{seoUrl}";
+                updated = true;
+            }
+
+            if (updated)
+                node.Update();
+        }
+    }
+}
---
+++ CMS/DynamicRoutingModule.cs
@@ -0,0 +1,28 @@
+﻿using CMS.DataEngine;
+using CMS.DocumentEngine;
+
+namespace DeleteBoilerplate.Modules
+{
+    public class DynamicRoutingModule : Module
+    {
+        public DynamicRoutingModule() : base("DynamicRouting")
+        {
+        }
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+
+            DocumentEvents.Update.Before += UpdateOnBefore;
+            DocumentEvents.Insert.Before += UpdateOnBefore;
+        }
+
+        private void UpdateOnBefore(object sender, DocumentEventArgs e)
+        {
+            var basePageType = DataClassInfoProvider.GetClasses()
+                .Where(dataClass => string.Equals(dataClass.Name, BasePage.CLASS_NAME))
+
+            throw new System.NotImplementedException();
+        }
+    }
+}
