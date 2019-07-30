c7bba5894c69662e26cdaa832aeca59c5161e070
Alexey Nenazhenko <alexey.nenazhenko@deleteagency.com>
2019-07-30 17:41:59 +0500

    added generic components project and base widget controller

---
+++ Websites/Infrastructure/DeleteBoilerplate.DynamicRouting/Controllers/BaseWidgetController.cs
@@ -0,0 +1,8 @@
+﻿using Kentico.PageBuilder.Web.Mvc;
+
+namespace DeleteBoilerplate.DynamicRouting.Controllers
+{
+    public abstract class BaseWidgetController<TPropertiesType> : WidgetController<TPropertiesType> where TPropertiesType : class, IWidgetProperties, new()
+    {
+    }
+}
