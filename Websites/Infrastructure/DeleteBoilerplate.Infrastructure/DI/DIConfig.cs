d1f10ade5dc183b85b69cf054a8b54ddfd14518f
Alexey Nenazhenko <alexey.nenazhenko@deleteagency.com>
2019-07-31 11:31:46 +0500

    downgrade references to Kentico.Libraries versions *in progress

--- Websites/Infrastructure/DeleteBoilerplate.Infrastructure/DI/DIConfig.cs
+++ Websites/Infrastructure/DeleteBoilerplate.Infrastructure/DI/DIConfig.cs
@@ -1,4 +1,8 @@
-﻿using AutoMapper;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using AutoMapper;
 using DeleteBoilerplate.Infrastructure.Helpers;
 using LightInject;
 
@@ -8,20 +12,44 @@ namespace DeleteBoilerplate.Infrastructure
     {
         public static void Bootstrap()
         {
-            var container = new ServiceContainer();
-
-            var assemblies = AssemblyHelper.GetDiscoverableAssemblyAssemblies();
-            foreach (var assembly in assemblies)
+            try
             {
-                container.RegisterAssembly(assembly);
+                //The code that causes the error goes here.
+                var container = new ServiceContainer();
+
+                var assemblies = AssemblyHelper.GetDiscoverableAssemblyAssemblies();
+                foreach (var assembly in assemblies)
+                {
+                    container.RegisterAssembly(assembly);
+                }
+                container.RegisterControllers(assemblies);
+
+                container.EnableMvc();
+                container.EnableAnnotatedPropertyInjection();
+
+                container.RegisterInstance<MapperConfiguration>(AutoMapperConfig.BuildMapperConfiguration(container));
+                container.Register<IMapper>(c => new Mapper(c.GetInstance<MapperConfiguration>(), c.GetInstance));
             }
-            container.RegisterControllers(assemblies);
-
-            container.EnableMvc();
-            container.EnableAnnotatedPropertyInjection();
-
-            container.RegisterInstance<MapperConfiguration>(AutoMapperConfig.BuildMapperConfiguration(container));
-            container.Register<IMapper>(c => new Mapper(c.GetInstance<MapperConfiguration>(),c.GetInstance));
+            catch (ReflectionTypeLoadException ex)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (Exception exSub in ex.LoaderExceptions)
+                {
+                    sb.AppendLine(exSub.Message);
+                    FileNotFoundException exFileNotFound = exSub as FileNotFoundException;
+                    if (exFileNotFound != null)
+                    {
+                        if (!string.IsNullOrEmpty(exFileNotFound.FusionLog))
+                        {
+                            sb.AppendLine("Fusion Log:");
+                            sb.AppendLine(exFileNotFound.FusionLog);
+                        }
+                    }
+                    sb.AppendLine();
+                }
+                string errorMessage = sb.ToString();
+                //Display or log the error based on your application.
+            }
         }
 
     }
