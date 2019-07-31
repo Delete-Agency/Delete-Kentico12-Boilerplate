430296a4470373acecc38cb1a44f1b1e17dd3c5e
Alexey Nenazhenko <alexey.nenazhenko@deleteagency.com>
2019-07-31 11:51:07 +0500

    finish downgrade

--- Websites/Infrastructure/DeleteBoilerplate.Infrastructure/DI/DIConfig.cs
+++ Websites/Infrastructure/DeleteBoilerplate.Infrastructure/DI/DIConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
-using System.Text;
-using AutoMapper;
+﻿using AutoMapper;
 using DeleteBoilerplate.Infrastructure.Helpers;
 using LightInject;
 
@@ -12,9 +8,6 @@ namespace DeleteBoilerplate.Infrastructure
     {
         public static void Bootstrap()
         {
-            try
-            {
-                //The code that causes the error goes here.
                 var container = new ServiceContainer();
 
                 var assemblies = AssemblyHelper.GetDiscoverableAssemblyAssemblies();
@@ -29,28 +22,6 @@ namespace DeleteBoilerplate.Infrastructure
 
                 container.RegisterInstance<MapperConfiguration>(AutoMapperConfig.BuildMapperConfiguration(container));
                 container.Register<IMapper>(c => new Mapper(c.GetInstance<MapperConfiguration>(), c.GetInstance));
-            }
-            catch (ReflectionTypeLoadException ex)
-            {
-                StringBuilder sb = new StringBuilder();
-                foreach (Exception exSub in ex.LoaderExceptions)
-                {
-                    sb.AppendLine(exSub.Message);
-                    FileNotFoundException exFileNotFound = exSub as FileNotFoundException;
-                    if (exFileNotFound != null)
-                    {
-                        if (!string.IsNullOrEmpty(exFileNotFound.FusionLog))
-                        {
-                            sb.AppendLine("Fusion Log:");
-                            sb.AppendLine(exFileNotFound.FusionLog);
-                        }
-                    }
-                    sb.AppendLine();
-                }
-                string errorMessage = sb.ToString();
-                //Display or log the error based on your application.
-            }
         }
-
     }
 }
