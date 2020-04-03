2be570c072d0e501e06a530ae8af89d8de91a307
Pavel Ivanov <pavel.ivanov@deleteagency.com>
2020-04-04 00:14:57 +0500

    Add identity config

---
+++ Websites/WebApps/DeleteBoilerplate/Features/DeleteBoilerplate.Account/App_Start/IdentityConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using CMS.Helpers;
+using CMS.SiteProvider;
+using Kentico.Membership;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
+using DeleteBoilerplate.Account.Infrastructure;
+
+[assembly: OwinStartup(typeof(DeleteBoilerplate.Account.IdentityConfig))]
+
+namespace DeleteBoilerplate.Account
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<KenticoUserManager<AppUser>>(() =>
+            {
+                var userManager = new KenticoUserManager<AppUser>(new KenticoUserStore<AppUser>(SiteContext.CurrentSiteName));
+                userManager.UserValidator = new UserValidator<AppUser, int>(userManager)
+                {
+                    AllowOnlyAlphanumericUserNames = true,
+                    RequireUniqueEmail = true
+                };
+                var provider = new DpapiDataProtectionProvider(Domain.Constants.Identity.AppName);
+                userManager.UserTokenProvider =
+                    new DataProtectorTokenProvider<AppUser, int>(provider.Create(Domain.Constants.Identity.AppTokenName)) { TokenLifespan = TimeSpan.FromDays(1)};
+                userManager.EmailService = new EmailService();
+                return userManager;
+            });
+
+            app.CreatePerOwinContext<KenticoSignInManager<AppUser>>(KenticoSignInManager<AppUser>.Create);
+
+            UrlHelper urlHelper = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                CookieName = DeleteBoilerplate.Domain.Constants.Identity.AppCookieName,
+                CookieSecure = CookieSecureOption.SameAsRequest,
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString(urlHelper.Action("Login", "Account")),
+                CookieManager = new SystemWebCookieManager(),
+                Provider = new CookieAuthenticationProvider
+                {
+                    OnApplyRedirect = context =>
+                        context.Response.Redirect(urlHelper.Action("Login", "Account") + new Uri(context.RedirectUri).Query)
+                }
+            });
+            CookieHelper.RegisterCookie(DefaultAuthenticationTypes.ApplicationCookie, CookieLevel.Essential);
+        }
+    }
+}
