8d05f9ac1cd529dfeedbd1b72da922b522214131
Pavel Ivanov <pavel.ivanov@deleteagency.com>
2020-04-03 23:38:31 +0500

    Create account project

---
+++ Websites/WebApps/DeleteBoilerplate/Features/DeleteBoilerplate.Account/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace DeleteBoilerplate.Account.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View("~/Views/Account/Register.cshtml");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("~/Views/Account/Login.cshtml");
+        }
+    }
+}
---
+++ Websites/WebApps/DeleteBoilerplate/Features/DeleteBoilerplate.Account/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DeleteBoilerplate.Account.MvcApplication" Language="C#" %>
