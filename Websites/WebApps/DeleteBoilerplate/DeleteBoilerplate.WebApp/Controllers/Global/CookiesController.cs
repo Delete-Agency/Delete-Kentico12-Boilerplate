d2c50cce8597fa148b05af3180e2ab73fb586933
Alexey Nenazhenko <alexey.nenazhenko@deleteagency.com>
2020-04-21 19:28:41 +0500

    add campaign resolving on cookies consent, fix current contact resolving on 1st request

--- Websites/WebApps/DeleteBoilerplate/DeleteBoilerplate.WebApp/Controllers/Global/CookiesController.cs
+++ Websites/WebApps/DeleteBoilerplate/DeleteBoilerplate.WebApp/Controllers/Global/CookiesController.cs
@@ -5,8 +5,12 @@ using DeleteBoilerplate.WebApp.Constants;
 using DeleteBoilerplate.WebApp.Models.Global.Cookies;
 using LightInject;
 using System;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using CMS.Core;
+using CMS.SiteProvider;
+using CMS.WebAnalytics;
 
 namespace DeleteBoilerplate.WebApp.Controllers.Global
 {
@@ -17,12 +21,19 @@ namespace DeleteBoilerplate.WebApp.Controllers.Global
         [Inject]
         protected ICurrentCookieLevelProvider CookieLevelProvider { get; set; }
 
+        protected ICampaignService CampaignService { get; set; }
+
+        public CookiesController()
+        {
+            this.CampaignService = Service.Resolve<ICampaignService>();
+        }
+
         [HttpGet]
         public ActionResult Cookies()
         {
-            int level = this.CookieLevelProvider.GetCurrentCookieLevel();
+            var level = this.CookieLevelProvider.GetCurrentCookieLevel();
 
-            var model = new CookiesViewModel()
+            var model = new CookiesViewModel
             {
                 IsPerformance = (level == CookieLevel.Essential) || (level == CookieLevel.All),
                 IsTracking = (level == CookieLevel.All),
@@ -36,12 +47,14 @@ namespace DeleteBoilerplate.WebApp.Controllers.Global
         }
 
         [HttpPost]
-        public JsonResult AcceptAllCookies()
+        public JsonResult AcceptAllCookies(Uri pageUrl)
         {
             try
             {
                 CookieLevelProvider.SetCurrentCookieLevel(CookieLevel.All);
                 this.ConfirmationCookieAddedForYear();
+
+                this.AssignCampaignIfExist(pageUrl);
 
                 return Json(new { Result = true }, JsonRequestBehavior.AllowGet);
             }
@@ -55,15 +68,16 @@ namespace DeleteBoilerplate.WebApp.Controllers.Global
         /// <summary>
         /// Set a specific level of cookies
         /// </summary>
+        /// <param name="pageUrl">Url of the page where cookie consent was accepted</param>
         /// <param name="isPerformance">Cookie level 2 (Essential/0)</param>
         /// <param name="isTracking">Cookie level 3 (All/1000)</param>
         /// <returns></returns>
         [HttpPost]
-        public JsonResult SetCookieSettings(bool isPerformance = false, bool isTracking = false)
+        public JsonResult SetCookieSettings(Uri pageUrl, bool isPerformance = false, bool isTracking = false)
         {
             try
             {
-                int cookieLevel = CookieLevel.System;
+                var cookieLevel = CookieLevel.System;
 
                 if (isTracking)
                 {
@@ -77,6 +91,9 @@ namespace DeleteBoilerplate.WebApp.Controllers.Global
                 CookieLevelProvider.SetCurrentCookieLevel(cookieLevel);
                 this.ConfirmationCookieAddedForYear();
 
+                if (cookieLevel >= CookieLevel.Visitor)
+                    this.AssignCampaignIfExist(pageUrl);
+
                 return Json(new { Result = true }, JsonRequestBehavior.AllowGet);
             }
             catch (Exception ex)
@@ -87,9 +104,22 @@ namespace DeleteBoilerplate.WebApp.Controllers.Global
 
         }
 
+        private void AssignCampaignIfExist(Uri pageUrl)
+        {
+            var queryParams = pageUrl.ParseQueryString();
+
+            var campaignCode = queryParams["utm_campaign"];
+            if (!string.IsNullOrEmpty(campaignCode))
+            {
+                var source = queryParams["utm_source"];
+                var content = queryParams["utm_content"];
+                this.CampaignService.SetCampaign(campaignCode, SiteContext.CurrentSite.SiteName, source, content);
+            }
+        }
+
         private void ConfirmationCookieAddedForYear()
         {
-            HttpCookie cookie = new HttpCookie(CookiesConstants.CookiesMark)
+            var cookie = new HttpCookie(CookiesConstants.CookiesMark)
             {
                 Value = Boolean.TrueString.ToLower(),
                 Expires = DateTime.Now.AddYears(1)
