86c116274d9ec337f5d26090ed5f75cfedf15bcb
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2020-04-24 17:44:43 +0500

    add validation for kentico form

--- Websites/WebApps/DeleteBoilerplate/Features/DeleteBoilerplate.Forms/Controllers/BizFormController.cs
+++ Websites/WebApps/DeleteBoilerplate/Features/DeleteBoilerplate.Forms/Controllers/BizFormController.cs
@@ -4,6 +4,7 @@ using CMS.EventLog;
 using CMS.Helpers;
 using CMS.OnlineForms;
 using CMS.SiteProvider;
+using DeleteBoilerplate.Common.Extensions;
 using DeleteBoilerplate.Forms.Models;
 using Kentico.Forms.Web.Mvc;
 using LightInject;
@@ -31,37 +32,33 @@ namespace DeleteBoilerplate.Forms.Controllers
         {
             try
             {
-                if (ModelState.IsValid == false)
+                var formInfo = BizFormInfoProvider.GetBizFormInfo(formData.FormName, SiteContext.CurrentSiteID);
+                var formComponents = this.BindFormComponents(formInfo, formData.ElementId);
+
+                bool isValidModel = this.IsValidAndUpdateModelState(formComponents, formData.ElementId);
+                if (isValidModel == false)
                 {
-                    string errorMessage = ResHelper.GetString("DeleteBoilerplate.Forms.BizFormName.InvalidData");
-                    return Json(new { Result = false, errorMessage }, JsonRequestBehavior.AllowGet);
+                    return this.ValidationErrorResult();
                 }
 
-                var formInfo = BizFormInfoProvider.GetBizFormInfo(formData.FormName, SiteContext.CurrentSiteID);
-                var formItem = this.GetBizFormItem(formInfo, formData.ElementId);
+                var formItem = this.GetBizFormItem(formInfo, formComponents, formData.ElementId);
 
                 formItem.SubmitChanges(false);
                 this.FormProvider.SendEmails(formInfo, formItem);
 
-                string redirect = formInfo.FormRedirectToUrl ?? string.Empty;
-                string displayText = formInfo.FormDisplayText ?? string.Empty;
-
-                return Json(new { Result = true, Redirect = redirect, DisplayText = displayText }, JsonRequestBehavior.AllowGet);
+                return this.SuccessfulResult(formInfo);
             }
             catch (Exception ex)
             {
                 EventLogProvider.LogException($"{BizFormName} Submit", ex.ToString(), null);
 
-                string errorMessage = ResHelper.GetString("DeleteBoilerplate.Forms.BizFormName.Error");
-                return Json(new { Result = false, errorMessage }, JsonRequestBehavior.AllowGet);
+                return this.ServerErrorResult();
             }
         }
 
-        private BizFormItem GetBizFormItem(BizFormInfo formInfo, string elementId)
+        private BizFormItem GetBizFormItem(BizFormInfo formInfo, List<FormComponent> formComponents, string elementId)
         {
-            var formComponents = this.BindFormComponents(formInfo, elementId);
-
-            BizFormItem formItem;
+             BizFormItem formItem;
 
             var isExistBizFormItem = BizFormItemProvider.GetItems(DataClassInfoProvider.GetClassName(formInfo.FormClassID))
                 .HasExistingItemForContact(formInfo, ContactManagementContext.CurrentContact?.ContactGUID, out formItem);
@@ -89,6 +86,100 @@ namespace DeleteBoilerplate.Forms.Controllers
 
             var formComponents = modelBinder.BindModel(ControllerContext, modelBindingContext) as List<FormComponent>;
             return formComponents;
+        }
+        
+        private bool IsValidAndUpdateModelState(List<FormComponent> formComponents, string elementId)
+        {
+            this.ModelState.Clear();
+
+            foreach (var item in formComponents)
+            {
+                this.AddRequiredErrors(item, elementId);
+
+                this.AddRuleErrors(item, elementId);
+            }
+
+            bool isValidModel = this.ModelState.Count <= 0 ;
+            return isValidModel;
+        }
+
+        private void AddRequiredErrors(FormComponent item, string elementId)
+        {
+            //ToDo: It needs refactoring or thinking
+            if (item.BaseProperties.Required)
+            {
+                string itemTypeName = item.GetType().Name;
+
+                if (itemTypeName.Equals(typeof(IntInputComponent).Name))
+                {
+                    var val = item.GetObjectValue() as int?;
+                    if (val == null)
+                        this.AddErrorInModelState(item, elementId, "IntInputComponent");
+                }
+                else if (itemTypeName.Equals(typeof(TextInputComponent).Name))
+                {
+                    var val = item.GetObjectValue() as string;
+                    if (val.IsNullOrEmpty())
+                        this.AddErrorInModelState(item, elementId, "TextInputComponent");
+                }
+                else if (itemTypeName.Equals(typeof(EmailInputComponent).Name))
+                {
+                    var val = item.GetObjectValue() as string;
+                    if (val.IsNullOrEmpty())
+                        this.AddErrorInModelState(item, elementId, "EmailInputComponent");
+                }
+                else if (itemTypeName.Equals(typeof(CheckBoxComponent).Name))
+                {
+                    var val = item.GetObjectValue() as bool?;
+                    if (val != true)
+                        this.AddErrorInModelState(item, elementId, "CheckBoxComponent");
+                }
+                else if (itemTypeName.Equals(typeof(TextAreaComponent).Name))
+                {
+                    var val = item.GetObjectValue() as bool?;
+                    if (val != true)
+                        this.AddErrorInModelState(item, elementId, "TextAreaComponent");
+                }
+            }
+        }
+
+        private void AddRuleErrors(FormComponent item, string elementId)
+        {
+            foreach (var rule in item.BaseProperties.ValidationRuleConfigurations)
+            {
+                bool isValid = rule.ValidationRule.IsValueValid(item.GetObjectValue());
+                if (isValid == false)
+                {
+                    this.AddErrorInModelState(item, elementId, rule.ValidationRule.ErrorMessage);
+                }
+            }
+        }
+
+        private void AddErrorInModelState(FormComponent item, string elementId, string errorMessage)
+        {
+            ModelState.AddModelError($"{elementId}.{item.Name}.{item.LabelForPropertyName}", errorMessage);
+        }
+
+        protected virtual ActionResult SuccessfulResult(BizFormInfo formInfo)
+        {
+            string redirect = formInfo.FormRedirectToUrl ?? string.Empty;
+            string displayText = formInfo.FormDisplayText ?? string.Empty;
+
+            return Json(new { Result = true, Redirect = redirect, DisplayText = displayText }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected virtual ActionResult ValidationErrorResult()
+        {
+            this.Response.StatusCode = 422;
+            return this.Json(FormResponseModel.BuildValidationErrorResponse(this.ModelState));
+        }
+
+        protected virtual ActionResult ServerErrorResult()
+        {
+            this.Response.StatusCode = 500;
+
+            string errorMessage = ResHelper.GetString("DeleteBoilerplate.Forms.BizFormName.Error");
+            return Json(new { Result = false, errorMessage }, JsonRequestBehavior.AllowGet);
         }
     }
 }
