05d633646b70df638091e16805fe99dc5f27a1f3
Iaroslavz <iaroslav.zaiarnyi@deleteagency.com>
2020-05-14 17:35:24 +0500

    remove frontend json formatter

--- Websites/WebApps/DeleteBoilerplate/DeleteBoilerplate.WebApp/assets/components/twitter-feed/js/twitter-feed.component.js
+++ Websites/WebApps/DeleteBoilerplate/DeleteBoilerplate.WebApp/assets/components/twitter-feed/js/twitter-feed.component.js
@@ -4,34 +4,6 @@ import React from 'preact/compat';
 import Tweets from 'components/twitter-feed/js/tweets';
 import api from 'general/js/api';
 
-function toCamel(o) {
-    let newO;
-    let origKey;
-    let newKey;
-    let value;
-    if (o instanceof Array) {
-        return o.map(function (value) {
-            if (typeof value === 'object') {
-                value = toCamel(value);
-            }
-            return value;
-        });
-    }
-    newO = {};
-    for (origKey in o) {
-        if (o.hasOwnProperty(origKey)) {
-            newKey = (origKey.charAt(0).toLowerCase() + origKey.slice(1) || origKey).toString();
-            value = o[origKey];
-            if (value instanceof Array || (value !== null && value.constructor === Object)) {
-                value = toCamel(value);
-            }
-            newO[newKey] = value;
-        }
-    }
-
-    return newO;
-}
-
 export default class TwitterFeed extends DcBaseComponent {
     static getNamespace() {
         return 'twitter-feed';
@@ -43,10 +15,7 @@ export default class TwitterFeed extends DcBaseComponent {
 
     _requestContent = () => {
         api.get(this.options.endpoint).then((result) => {
-            render(
-                <Tweets tweets={toCamel(result.data.Tweets)} parentNode={this.element} />,
-                this.element
-            );
+            render(<Tweets tweets={result.data.data} parentNode={this.element} />, this.element);
         });
     };
 }
