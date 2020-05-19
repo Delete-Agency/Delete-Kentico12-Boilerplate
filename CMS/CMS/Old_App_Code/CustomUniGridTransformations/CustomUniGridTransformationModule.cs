6cd2cdbe2f522c8d98b299469a1b2f90d7efdf8b
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2020-05-19 15:39:14 +0500

    add Custom UniGrid Transformation Module

---
+++ CMS/CMS/Old_App_Code/CustomUniGridTransformations/CustomUniGridTransformationModule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using CMS;
+using CMS.Core;
+using CMS.DataEngine;
+using CMS.SiteProvider;
+using CMS.UIControls;
+using CMSApp.Old_App_Code.CustomUniGridTransformations;
+using DeleteBoilerplate.Domain.Models.CmsClasses.SitemapRule;
+
+[assembly: RegisterModule(typeof(CustomUniGridTransformationModule))]
+
+namespace CMSApp.Old_App_Code.CustomUniGridTransformations
+{
+    public class CustomUniGridTransformationModule : Module
+    {
+        public CustomUniGridTransformationModule()
+            : base("CustomUniGridTransformations")
+        {
+        }
+
+        public CustomUniGridTransformationModule(ModuleMetadata metadata, bool isInstallable = false) : base(metadata, isInstallable)
+        {
+        }
+
+        public CustomUniGridTransformationModule(string moduleName, bool isInstallable = false) : base(moduleName, isInstallable)
+        {
+        }
+
+        protected override void OnInit()
+        {
+
+            base.OnInit();
+
+            UniGridTransformations.Global.RegisterTransformation("#sitemapruletype", GetSitemapRuleType);
+            UniGridTransformations.Global.RegisterTransformation("#siteid", GetSiteNameById);
+        }
+
+        private static object GetSitemapRuleType(object parameter)
+        {
+            SitemapRuleType sitemapRule = (SitemapRuleType)Enum.ToObject(typeof(SitemapRuleType), parameter);
+            return Enum.GetName(typeof(SitemapRuleType), sitemapRule);
+        }
+
+        private static object GetSiteNameById(object parameter)
+        {
+            var isValid = Int32.TryParse(parameter.ToString(), out int siteId);
+            if (isValid && siteId > 0)
+            {
+                var siteName = SiteInfoProvider.GetSiteName(siteId);
+                return siteName;
+            }
+
+            return string.Empty;
+        }
+    }
+}
