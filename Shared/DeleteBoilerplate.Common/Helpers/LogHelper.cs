07525206ba0a62d510b7fac4ea8c7b90a71b5e11
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2020-05-19 18:26:03 +0500

    refactoring and add checks for Log Helper

--- Shared/DeleteBoilerplate.Common/Helpers/LogHelper.cs
+++ Shared/DeleteBoilerplate.Common/Helpers/LogHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Web;
 using CMS.EventLog;
 
@@ -6,48 +7,41 @@ namespace DeleteBoilerplate.Common.Helpers
 {
     public static class LogHelper
     {
-        public static void LogError(string source, string evenCode, string description)
-        {
-            EventLogProvider.LogEvent(EventType.ERROR, source, evenCode, description);
-        }
+        private const int SymbolsLimitInDB = 100;
 
-        public static void LogError(string source, string evenCode, string description, int siteId)
+        public static void LogError(string source, string evenCode, string description, int siteId = 0)
         {
             EventLogProvider.LogEvent(EventType.ERROR, source, evenCode, description, siteId: siteId);
         }
 
-        public static void LogException(Exception ex)
+        public static void LogException(Exception ex, int siteId = 0)
         {
-            EventLogProvider.LogException($"{ex.TargetSite?.DeclaringType?.Name}.{ex.TargetSite?.Name}()", ex.Message, ex);
+           AddLogException($"{ex.TargetSite?.DeclaringType?.Name}.{ex.TargetSite?.Name}()", ex.Message, ex, siteId);
         }
 
-        public static void LogException(string eventCode, Exception ex)
+        public static void LogException(string eventCode, Exception ex, int siteId = 0)
         {
-            EventLogProvider.LogException($"{ex.TargetSite?.DeclaringType?.Name}.{ex.TargetSite?.Name}()", eventCode, ex);
+           AddLogException($"{ex.TargetSite?.DeclaringType?.Name}.{ex.TargetSite?.Name}()", eventCode, ex, siteId);
         }
 
-        public static void LogException(string eventCode, Exception ex, int siteId)
+        public static void LogException(string source, string evenCode, Exception ex, int siteId = 0)
         {
-            EventLogProvider.LogException($"{ex.TargetSite?.DeclaringType?.Name}.{ex.TargetSite?.Name}()", eventCode, ex, siteId);
+            AddLogException(source, evenCode, ex, siteId);
         }
 
-        public static void LogException(string source, string evenCode, Exception ex)
-        {
-            EventLogProvider.LogException(source, evenCode, ex);
-        }
 
-        public static void LogException(HttpRequestBase request, Exception ex)
+        public static void LogException(HttpRequestBase request, Exception ex, int siteId = 0)
         {
             var controllerName = GetControllerAndActionName(request, out string actionName);
 
-            EventLogProvider.LogException($"{controllerName}.{actionName}()", ex.Message, ex);
+            AddLogException($"{controllerName}.{actionName}()", ex.Message, ex, siteId);
         }
 
-        public static void LogException(HttpRequestBase request, string evenCode, Exception ex)
+        public static void LogException(HttpRequestBase request, string evenCode, Exception ex, int siteId = 0)
         {
             var controllerName = GetControllerAndActionName(request, out string actionName);
 
-            EventLogProvider.LogException($"{controllerName}.{actionName}()", evenCode, ex);
+            AddLogException($"{controllerName}.{actionName}()", evenCode, ex, siteId);
         }
 
         private static string GetControllerAndActionName(HttpRequestBase request, out string actionName)
@@ -56,6 +50,14 @@ namespace DeleteBoilerplate.Common.Helpers
             actionName = request.RequestContext.RouteData.Values["action"]?.ToString() ?? string.Empty;
             
             return controllerName;
+        }
+
+        private static void AddLogException(string source, string evenCode, Exception ex, int siteId = 0)
+        {
+            var validSource = new string(source.Take(SymbolsLimitInDB).ToArray());
+            var validEventCode = new string(evenCode.Take(SymbolsLimitInDB).ToArray());
+        
+            EventLogProvider.LogException(validSource, validEventCode, ex, siteId);
         }
     }
 }
