34499abe49eec76d7fc4af70f09d15727f2be44b
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2020-05-20 17:46:20 +0500

    add Json and Exception extensions

---
+++ Shared/DeleteBoilerplate.Common/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace DeleteBoilerplate.Common.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static string GetAllMessages(this Exception exception)
+        {
+            var currentException = exception;
+            var str = new StringBuilder();
+
+            while (currentException != null)
+            {
+                str.AppendLine(currentException.Message);
+                currentException = currentException.InnerException;
+            }
+            return str.ToString();
+        }
+
+    }
+}
---
+++ Shared/DeleteBoilerplate.Common/Extensions/JsonExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+
+namespace DeleteBoilerplate.Common.Extensions
+{
+    public static class JsonExtensions
+    {
+        public static JsonSerializerSettings DefaultJsonSerializerSettings => new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Include,
+            DateFormatString = "yyyy-MM-ddTHH:mm:ss",
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        public static TObject DeserializeJson<TObject>(this string json)
+        {
+            return json.DeserializeJson<TObject>(DefaultJsonSerializerSettings);
+        }
+
+        public static TObject DeserializeJson<TObject>(this string json, JsonSerializerSettings jsonSerializerSettings)
+        {
+            return JsonConvert.DeserializeObject<TObject>(json, jsonSerializerSettings);
+        }
+
+        public static object DeserializeJson(this string json, Type objectType)
+        {
+            return json.DeserializeJson(objectType, DefaultJsonSerializerSettings);
+        }
+
+        public static object DeserializeJson(this string json, Type objectType, JsonSerializerSettings jsonSerializerSettings)
+        {
+            return JsonConvert.DeserializeObject(json, objectType, jsonSerializerSettings);
+        }
+
+    }
+}
